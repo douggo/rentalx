587311169107ea22b9c31341aa86b4825ca9ff1a
Douglas Felipe da Silva <douglas.silva@unidavi.edu.br>
2022-05-30 16:01:28 -0300

    creating `implementations` folder to move all repos created there

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from '../../repositories/CategoriesRespository';
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRespository';
 import { CreateCategoryController } from './CreateCategoryController';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from '../../repositories/CategoriesRespository';
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRespository';
 import { ListCategoriesController } from './ListCategoriesController';
 import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
