03864887e02edb6ca49bcdc00a80cc4dbb158444
Douglas Felipe da Silva <douglas.silva@unidavi.edu.br>
2022-05-30 16:50:58 -0300

    creating use cases for creating and listing all specifications of cars

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,13 +1,12 @@
-import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository';
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateSpecificationService {
+class CreateSpecificationUseCase {
   constructor(private specificationsRepository: ISpecificationsRepository) {}
-
   execute({ name, description }: IRequest): void {
     if (this.specificationsRepository.findByName(name)) {
       throw new Error('Specification already exists!');
@@ -16,4 +15,4 @@ class CreateSpecificationService {
   }
 }
 
-export { CreateSpecificationService };
+export { CreateSpecificationUseCase };
